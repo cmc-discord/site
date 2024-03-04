923b2dbe38a53595715220104e7fffcb565b7ec5
Gareth Coles <gdude2002@gmail.com>
2024-03-04 19:20:50 +0000

    Markdown headers

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,8 +1,25 @@
+import { h } from "hastscript";
 import { defineMDSveXConfig } from "mdsvex";
+
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 export default defineMDSveXConfig({
 	extensions: [".svelte.md", ".md", ".svx"],
+	rehypePlugins: [
+		// Header linking
+		rehypeSlug,
+		[
+			rehypeAutolinkHeadings,
+			{
+				content: h("span.header-link-icon", "🔗"),
+				behavior: "append",
+				properties: { title: "Link to heading", ariaHidden: true, tabIndex: -1, dataPagefindIgnore: "" }
+			}
+		],
+	],
+
 	layout: {
-		"_": "/src/lib/markdown-layout.svelte"
-	}
-})
+		"_": "/src/lib/markdown-layout.svelte",
+	},
+});
