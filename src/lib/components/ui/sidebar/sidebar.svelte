decb63f1b2dcde15f708358b55e6444a7ea656f6
Gareth Coles <gdude2002@gmail.com>
2024-03-05 17:30:53 +0000

    Remove sidebar placeholders

--- src/lib/components/ui/sidebar/sidebar.svelte
+++ src/lib/components/ui/sidebar/sidebar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Sidebar from "./"
 	import { Separator } from "$lib/components/ui/separator";
-	import { Home, Tag, Wrench } from "lucide-svelte";
+	import { Home, MoreHorizontal, Tag, Wrench } from "lucide-svelte";
 </script>
 
 <div class="flex flex-col p-2 mt-1 overflow-y-auto h-full">
@@ -11,22 +11,35 @@
 		Home
 	</Sidebar.SectionPickerItem>
 
-	<Sidebar.SectionPickerItem href="#" disabled>
-		<Wrench size="1.5rem" slot="icon" />
-
-		Tools
-	</Sidebar.SectionPickerItem>
-
 	<Sidebar.SectionPickerItem href="/tags">
 		<Tag size="1.5rem" slot="icon" />
 
 		All Tags
 	</Sidebar.SectionPickerItem>
 
+	<!-- TODO: Make some tools!
+	<Sidebar.SectionPickerItem href="#" disabled>
+		<Wrench size="1.5rem" slot="icon" />
+
+		Tools
+	</Sidebar.SectionPickerItem>
+	-->
+
+	<Sidebar.SectionPickerItem href="#" disabled>
+		<MoreHorizontal size="1.5rem" slot="icon" />
+
+		Coming Soon...
+	</Sidebar.SectionPickerItem>
+
 	<Separator class="!my-3" />
 
 	<slot name="content"/>
 
+	<Sidebar.Link href="#" disabled>
+		Coming Soon...
+	</Sidebar.Link>
+
+	<!-- TODO: We have no content to put here yet.
 	<Sidebar.Link href="#" disabled>
 		Content 1
 	</Sidebar.Link>
@@ -62,4 +75,5 @@
 	<Sidebar.Link href="#" disabled>
 		Content 8
 	</Sidebar.Link>
+	-->
 </div>
